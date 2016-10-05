0dc65bee05e3d84e949e598ffb06ffb0f28ee6a9
haliphax <haliphax@gmail.com>
2016-10-05 18:36:03 -0500

    added files

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # repo2site
 
-Uses AJAX to built an HTML site from a git repo's MarkDown files
+Uses AJAX to build an HTML site from a git repo's MarkDown files
+
+## Usage
+
+Usage is seen in the `index.html` file. Replace the `<body>` tag's `onload`
+attribute so that it calls `repo2site` using your username and repo name.
+You may also specify the branch name (default: `master`) and the name of the
+initial file to load (default: `README.md`).
+
+This currently only works with GitHub, but could easily be adapted to other
+services by replacing the `stem` variable in `repo2site.js`.
+
+## Example
+
+The [x84-extras](https://x84-extras.github.io) site uses repo2site to generate
+its pages.
+
+## Credits
+
+I'm using [markdown-js](https://github.com/evilstreak/markdown-js) and a
+simple, cross-browser, vanilla JS AJAX implementation I found on
+[Krasimir Tsonev's blog](http://krasimirtsonev.com/blog/article/Cross-browser-handling-of-Ajax-requests-in-absurdjs)
+(which is used in AbsurdJS).
---
+++ js/repo2site.js
@@ -0,0 +1,75 @@
+function repo2site(repo, branch, readme)
+{
+	if (typeof branch === 'undefined') branch = 'master';
+	if (typeof readme === 'undefined') readme = 'README.md';
+
+	var stem = 'https://raw.githubusercontent.com/'
+		+ repo + '/' + branch + '/' ;
+
+	// get markdown page via AJAX and handle translation
+	function getPage(href) {
+		Ajax
+			.request({
+				url: stem + href
+				, method: 'get'
+			})
+			.done(function(res) {
+				var html = markdown.toHTML(res, 'Maruku')
+					// handle peculiar HTML for python code blocks
+					.replace(/<p><code>python\n((?:.|\n)+?)<\/code><\/p>/ig,
+						'<pre><code>$1</code></pre>')
+				;
+
+				history.pushState({ url: href }, '', '#/' + href);
+				document.body.innerHTML = html;
+				document.title = document.querySelector('h1').innerText;
+
+				// add "homepage" link if not README
+				if (href !== readme) {
+					document.body.innerHTML =
+						'<p><a href="' + readme + '">Homepage</a></p>'
+						+ document.body.innerHTML;
+				}
+
+				onLoad(null);
+			})
+		;
+	}
+
+	// "back" button pressed; handle state transition
+	function popState(e)
+	{
+		getPage(history.state === null ? readme: history.state.url);
+	}
+
+	// link was clicked; load page via AJAX
+	function linkClicked(e)
+	{
+		getPage(this.getAttribute('href').replace(/^#\//, ''));
+		e.preventDefault();
+	}
+
+	// page loaded via AJAX; handle link translation and click binding
+	function onLoad(e)
+	{
+		var links = document.querySelectorAll('a[href$=".md"]');
+
+		for (var i = 0; i < links.length; i++) {
+			var href = links[i].getAttribute('href');
+
+			if (/^https?:\/\//i.exec(href)) continue;
+
+			links[i].setAttribute('href', '#/' + href);
+			links[i].addEventListener('click', linkClicked);
+		}
+	}
+
+	// has a page been specified? if not, fallback to README
+	if (! /^#\/.+/i.exec(location.hash) && history.state === null)
+		getPage(readme);
+	else
+		getPage(location.hash.replace(/^#\//i, ''));
+
+	// window bindings
+	addEventListener('popstate', popState);
+}
